40dad1520cbc653c4c463308574866c179692449
HJM <jungmzzang@naver.com>
2025-04-02 18:04:42 +0900

    메인페이지 복구

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import Index from './pages/Index/Index';
-import MainPage from './pages/MainPage/MainPage';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import useAuthStore from '../src/store/useAuthStore';
 import RouteFilter from './Components/filters/RouteFilter';
-import Mainpage from './pages/MainPage/MainPage.jsx';
+import Mainpage from './pages/MainPage/Mainpage.jsx';
 
 
 const App = () => {
@@ -19,9 +18,9 @@ const App = () => {
       <div className="app-container">
         <Routes>
           <Route path='/' element={<Index />}></Route>
-          {/* <Route element={<RouteFilter/>}> */}
+          <Route element={<RouteFilter/>}>
             <Route path='/mainpage/*' element={<Mainpage/>}></Route>
-          {/* </Route> */}
+          </Route>
         </Routes>
       </div>
   );
--- src/pages/MainPage/Mainpage.jsx
+++ src/pages/MainPage/Mainpage.jsx
@@ -1,12 +1,24 @@
+import React from 'react';
+import style from '../MainPage/Mainpage.module.css';
+import Header from '../../Components/Header.jsx';
+import Home from './Home.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainContent from './MainContent.jsx';
+
+const Mainpage = () => {
 
 
-const MainPage=()=>{
-
-
-    return(
-        <div>
-            test
+  return (
+    <div className={style.container}>
+        <Header />
+        <div className={style.mainpageContents}>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="maincontent/*" element={<MainContent/>}></Route>
+        </Routes>
         </div>
-    );
-}
-export default MainPage;
+    </div>
+  );
+};
+
+export default Mainpage;
