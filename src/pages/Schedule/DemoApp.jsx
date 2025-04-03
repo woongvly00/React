029c19968dcd0d0f55dbb203892f7bb2cee59d30
HJM <jungmzzang@naver.com>
2025-04-03 19:28:59 +0900

    before pull

--- src/pages/Schedule/DemoApp.jsx
+++ src/pages/Schedule/DemoApp.jsx
@@ -8,44 +8,24 @@ import { createEventId } from './event-utils'
 import calenderStyle from './DemoApp.module.css';
 import caxios from '../../Utils/caxios';
 
+
+
+
+
+
 export default function DemoApp() {
 
-  // 주말 보이기 여부 / 캘린더에 표시 중인 이벤트 목록
-  const [weekendsVisible, setWeekendsVisible] = useState(true)
-  const [currentEvents, setCurrentEvents] = useState([])
-
-  // 주말표시 on/off
-  function handleWeekendsToggle() {
-    setWeekendsVisible(!weekendsVisible)
+  function handleDateSelect(selectInfo) {
+    setSelectedInfo(selectInfo)
+    setIsModalOpen(true)
   }
 
-
-  // 캘린더 주요 기능
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedInfo, setSelectedInfo] = useState(null);
   const [newTitle, setNewTitle] = useState('');
   const [newContent, setNewContent] = useState('');
   const [event, setEvent] = useState({});
 
-  function handleDateSelect(selectInfo) {
-
-    setSelectedInfo(selectInfo)
-    setIsModalOpen(true)
-    // let title = prompt('Please enter a new title for your event')
-    // let calendarApi = selectInfo.view.calendar
-
-    // calendarApi.unselect() // clear date selection
-
-    // if (title) {
-    //   calendarApi.addEvent({
-    //     id: createEventId(),
-    //     title,
-    //     start: selectInfo.startStr,
-    //     end: selectInfo.endStr,
-    //     allDay: selectInfo.allDay
-    //   })
-    // }
-  }
 
   function handleAddEvent() {
     const calendarApi = selectedInfo.view.calendar
@@ -86,50 +66,28 @@ export default function DemoApp() {
   
 
 
-  // 이벤트 클릭시
-  function handleEventClick(clickInfo) {
-    const event = clickInfo.event;
-    const mouseX = clickInfo.jsEvent.clientX; 
-    const mouseY = clickInfo.jsEvent.clientY - 90; 
-    this.setState({ selectedEvent: event, mouseX, mouseY });
 
-  //   showEventDetails = () => {
-  //       const { selectedEvent, mouseX, mouseY } = this.state;
-  //       if (selectedEvent && mouseX !== undefined && mouseY !== undefined) {
-  //           console.log("Start Date:", selectedEvent.start);
-  //           console.log("End Date:", selectedEvent.end);
-            
-  //           const startDate = selectedEvent.start && format(selectedEvent.start, 'HH:mm', { timeZone: 'Asia/Seoul' });
-  //           const endDate = selectedEvent.end && format(selectedEvent.end, 'HH:mm', { timeZone: 'Asia/Seoul' });
-            
-  //           return (
-  //               <div className="event-details" style={{ position: 'absolute', left: mouseX, top: mouseY }}>
-  //                   <h2>{selectedEvent.title}</h2>
-  //                   <div className='time_place'>
-  //                   <p>장소: {selectedEvent.extendedProps && selectedEvent.extendedProps.location}</p> 
-  //                   <p>시간: {startDate} ~ {endDate}</p>
-  //                   </div>
-  //                   <button onClick={() => this.setState({ selectedEvent: null, mouseX: undefined, mouseY: undefined })}>닫기</button>
-  //               </div>
-  //           );
-  //       }
-  //       return null;
 
-  // }
-}
-
-  // 삭제 버튼 클릭시 삭제
-  function CancleBtnClick(deleteEvent) {
-    if (window.confirm(`Are you sure you want to delete the event '${deleteEvent.event.title}'`)) {
-      deleteEvent.event.remove()
+    // 삭제 버튼 클릭시 삭제
+    function CancleBtnClick(deleteEvent) {
+      if (window.confirm(`Are you sure you want to delete the event '${deleteEvent.event.title}'`)) {
+        deleteEvent.event.remove()
+      }
     }
-  }
 
+    // 주말 보이기 여부 / 캘린더에 표시 중인 이벤트 목록
+    const [weekendsVisible, setWeekendsVisible] = useState(true)
+    const [currentEvents, setCurrentEvents] = useState([])
 
-  // 이벤트 변경 사항 업데이트
-  function handleEvents(events) {
-    setCurrentEvents(events)
-  }
+    // 주말표시 on/off
+    function handleWeekendsToggle() {
+      setWeekendsVisible(!weekendsVisible)
+    }
+
+    // 이벤트 변경 사항 업데이트
+    function handleEvents(events) {
+      setCurrentEvents(events)
+    }
 
   return (
     <div className='demo-app'>
