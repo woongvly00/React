f7c3c48c4b77914a2ac7613d28095da48eff4c69
Xavier Choi <chaeins25@naver.com>
2025-04-03 12:30:02 +0900

    approval status on

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -15,7 +15,7 @@ const Index = () => {
 
     const handleLogin = (e)=>{
         e.preventDefault();
-        axios.post("http://10.10.55.69/auth/login", login).then(resp=>{
+        axios.post("http://10.10.55.22/auth/login", login).then(resp=>{
             setAuth(resp.data, login.id);
             navi("/mainpage");
         }).catch((error) => {
