df85ecd74c3109092ecec348760332a36d3d97ff
jeokaet <whgnldud0325@naver.com>
2025-04-09 20:44:54 +0900

    jeokaet/mypage 완

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -5,9 +5,11 @@ import axios from "../../axios/axiosConfig";
 // 조휘영
 const Mypage = () => {
     const [edit, setEdit] = useState(false);
-    const [formData, setFormData] = useState({});
+    const [formData, setFormData] = useState({});   
     const [userInfo, setUserInfo] = useState(null);
     const { userId, isInitialized } = useAuthStore();
+    const [profileImage, setProfileImage] = useState(null); // 미리보기용
+    const [profileFile, setProfileFile] = useState(null);   // 서버로 보낼 파일
 
     useEffect(() => {
         if (!isInitialized || !userId) return;
@@ -15,12 +17,15 @@ const Mypage = () => {
             .then(res => {
                 setUserInfo(res.data);
                 setFormData({
+                    emp_code_id: res.data.emp_code_id,
                     emp_email:res.data.emp_email,
                     emp_phone:res.data.emp_phone,
                     postcode: res.data.postcode,
                     address1: res.data.address1,
                     address2: res.data.address2,
                 });
+                const path = res.data.profileDTO?.profile_path;
+            setProfileImage(path ? `http://10.10.55.66${path}` : "/default-profile.png");
             })
             .catch(err => console.error("유저 정보 로딩 실패", err));
     }, [isInitialized, userId]);
@@ -42,19 +47,42 @@ const Mypage = () => {
             }
         }).open();
     };
+    const handleFileChange = (e) =>{
+        const file = e.target.files[0];
+        if(file){
+            setProfileImage(URL.createObjectURL(file));  //이게 미리보기
+            setProfileFile(file); //이게 전송
+        }
+    }
 
     const handleSave = () => {
-        axios.put("http://10.10.55.66/mypage/update", formData)
-            .then(() => {
-                setUserInfo(prev => ({ ...prev, ...formData }));
-                setEdit(false);
-                alert("수정이 완료되었습니다.");
-            })
-            .catch(err => {
-                console.error("수정 실패", err);
-                alert("수정 중 오류가 발생했습니다.");
-            });
+        const formDataToSend = new FormData();
+        const employeeBlob = new Blob(
+            [JSON.stringify(formData)], 
+            { type: "application/json" }
+        );
+        formDataToSend.append("data", employeeBlob);
+
+        if (profileFile) {
+            formDataToSend.append("profile", profileFile);
+        }
+
+        axios.put("http://10.10.55.66/mypage/update", formDataToSend, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(() => {
+            setUserInfo(prev => ({ ...prev, ...formData }));
+            setEdit(false);
+            alert("수정이 완료되었습니다.");
+        })
+        .catch(err => {
+            console.error("수정 실패", err);
+            alert("수정 중 오류가 발생했습니다.");
+        });
     };
+    
 
     if (!userInfo) {
         return <div>Loading...</div>;
@@ -64,6 +92,13 @@ const Mypage = () => {
         <div className={style.container}>
             <h2>마이페이지</h2>
             <div className={style.profileCard}>
+                {/* 프로필 */}
+                <div className={style.pro}>
+                    <img src={profileImage} alt="프로필" onError={(e) => e.target.src = "/default-profile.png"} className={style.profileImage} />
+                {edit && (
+                    <input type="file" accept="image/*" onChange={handleFileChange}/>
+                )}
+                </div>
                 <div className={style.row}>
                     <label>이름:</label>
                     <span>{userInfo.emp_name}</span>
