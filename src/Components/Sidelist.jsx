b10696e4f79bcfcf0a446815998c99192d76f564
HJM <jungmzzang@naver.com>
2025-04-11 16:51:05 +0900

    사이드리스트 수정함

--- src/Components/Sidelist.jsx
+++ src/Components/Sidelist.jsx
@@ -2,123 +2,119 @@ import React, { useState } from 'react';
 import sideliststyle from './Sidelist.module.css';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import AddCategory from '../pages/Schedule/AddCategory';
 import ScheduleList from '../pages/Schedule/ScheduleList';
-import Insapage from '../pages/insa/insapage';
-import Annaul from '../pages/insa/Annaul';
 import ResvSideList from '../pages/Reserve/ResvSideList';
 
 const Sidelist = () => {
   const location = useLocation();
-  const name = location.state?.name;
-  
+  const pathname = location.pathname;
+  const boardPaths = [
+    '/board',
+    '/write_button',
+    '/standard',
+    '/reference',
+    '/free',
+    '/club',
+    '/department',
+    '/business',
+    '/support'
+  ];
 
-  const renderContent = () => {
-    switch (name) {
+  const hrPaths =[
+    '/attend',
+    '/record'
+  ];
 
-      // 게시판 list
-      case 'board':
-        return (
-            <div className={sideliststyle.sideList}>
+
+
+  const BoardSidelist = () => (
+    <div className={sideliststyle.sideList}>
             <aside className={sideliststyle.sidebar}>
-              
-              
-        <div><Link to="/mainpage/maincontent/write_button" state={{ name: "board" }}><button className={sideliststyle.button}>글쓰기</button></Link></div>
-
-        <div className={sideliststyle.boardsidebar}>
-          <div className={sideliststyle.wholegasy}>게시판 전체보기</div>
-          <div><Link to="/mainpage/maincontent/standard" state={{ name: "board" }}>공지사항</Link></div>
-          <div><Link to="/mainpage/maincontent/reference" state={{ name: "board" }}>자료실</Link></div>
-          <div>전사 게시판
-            <li><Link to="/mainpage/maincontent/free" state={{ name: "board" }}>자유게시판</Link></li>
-            <li><Link to="/mainpage/maincontent/club" state={{ name: "board" }}>동아리게시판</Link></li>
-          </div>
-          <div>그룹 게시판
-            <li><Link to="/mainpage/maincontent/department" state={{ name: "board" }}>부서 게시판</Link></li>
-            <li><Link to="/mainpage/maincontent/business" state={{ name: "board" }}>거래처별 변경사항</Link></li>
-            <li><Link to="/mainpage/maincontent/support" state={{ name: "board" }}>업체교육/업무지원보고서</Link></li>
-          </div>
-    
-    
-            </div>
+            <div><Link to="/mainpage/maincontent/write_button"><button className={sideliststyle.button}>글쓰기</button></Link></div>
+              <div className={sideliststyle.boardsidebar}>
+                <div className={sideliststyle.wholegasy}>게시판 전체보기</div>
+                <div><Link to="/mainpage/maincontent/standard">공지사항</Link></div>
+                <div><Link to="/mainpage/maincontent/reference">자료실</Link></div>
+                <div>전사 게시판
+                  <li><Link to="/mainpage/maincontent/free">자유게시판</Link></li>
+                  <li><Link to="/mainpage/maincontent/club">동아리게시판</Link></li>
+                </div>
+                <div>그룹 게시판
+                  <li><Link to="/mainpage/maincontent/department">부서 게시판</Link></li>
+                  <li><Link to="/mainpage/maincontent/business">거래처별 변경사항</Link></li>
+                  <li><Link to="/mainpage/maincontent/support">업체교육/업무지원보고서</Link></li>
+                </div>
+              </div>
             </aside>
         </div>
-        );
+  );
 
-
-      // 전자결제 list
-      case 'approval':
-        return (
-          <div className={sideliststyle.sideList}>
+  const ApprovalSidelist = () => (
+    <div className={sideliststyle.sideList}>
             <aside className={sideliststyle.sidebar}>
       
-              <div><Link to="/mainpage/maincontent/approval/write" state={{ name: "approval" }}>
+              <div><Link to="/mainpage/maincontent/approval/write">
                 <button className={sideliststyle.button}>결재 작성</button>
               </Link></div>
       
               <div className={sideliststyle.boardsidebar}>
                 <div className={sideliststyle.wholegasy}>결재 문서함</div>
-                <div><Link to="/mainpage/maincontent/approval/pending" state={{ name: "approval" }}>내가 결재할 문서</Link></div>
-                <div><Link to="/mainpage/maincontent/approval/requested" state={{ name: "approval" }}>내가 상신한 문서</Link></div>
-                <div><Link to="/mainpage/maincontent/approval/complete" state={{ name: "approval" }}>완료된 문서</Link></div>
-                <div><Link to="/mainpage/maincontent/approval/rejected" state={{ name: "approval" }}>반려된 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/pending">내가 결재할 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/requested">내가 상신한 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/complete">완료된 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/rejected">반려된 문서</Link></div>
               </div>
       
               <div className={sideliststyle.boardsidebar}>
                 <div className={sideliststyle.wholegasy}>부서 문서함</div>
-                <div><Link to="/mainpage/maincontent/approval/department/referenced" state={{ name: "approval" }}>부서 참조 문서</Link></div>
-                <div><Link to="/mainpage/maincontent/approval/department/created" state={{ name: "approval" }}>부서 생산 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/department/referenced">부서 참조 문서</Link></div>
+                <div><Link to="/mainpage/maincontent/approval/department/created">부서 생산 문서</Link></div>
               </div>
       
             </aside>
           </div>
-        );
+  );
 
+  const ScheduleSidelist = () => (
+    <div className={sideliststyle.sideList}>
+      <div>
+        <ScheduleList />
+      </div>
+    </div>
+  );
 
+  const ReserveSidelist = () => (
+    <div className={sideliststyle.sideList}>
+      <ResvSideList />
+    </div>
+  );
 
-       // 인사관리 list
-       case 'hr':
-        return <div className={sideliststyle.sideList}>
-                <aside className={sideliststyle.sidebar}>
+  const HrSidelist = () => (
+    <div className={sideliststyle.sideList}>
+      <aside className={sideliststyle.sidebar}>
         <div className={sideliststyle.boardsidebar}>
           <div className={sideliststyle.wholegasy}>인사관리</div>
           <ul>
-            <li><Link to="/mainpage/maincontent/attend" state={{ name: "hr" }}>개인 근태관리</Link></li>
-            <li><Link to="/mainpage/maincontent/record" state={{ name: "hr" }}>휴가 / 출장 기록관리</Link></li>
+            <li><Link to="/mainpage/maincontent/hr/attend">개인 근태관리</Link></li>
+            <li><Link to="/mainpage/maincontent/hr/record">휴가 / 출장 기록관리</Link></li>
           </ul>
         </div>
       </aside>
-                 
-              </div>;  
-
-
-      // 일정 list
-      case 'schedule':
-        return <div className={sideliststyle.sideList}>
-          <div>
-            <ScheduleList />
-          </div>
-        </div>;
-
-
-
-      // 예약 list
-      case 'reserve':
-        return <div className={sideliststyle.sideList}>
-                <ResvSideList />
-              </div>;
+    </div>
+  );
       
+     
+      if (pathname.includes('/approval')) return <ApprovalSidelist />;
+      if (boardPaths.some(path => pathname.includes(path))) return <BoardSidelist />;
+      if (pathname.includes('/schedule')) return <ScheduleSidelist />;
+      if (pathname.includes('/reserve')) return <ReserveSidelist />;
+      if (pathname.includes('/hr')) return <HrSidelist />;
 
-      // 디폴트값
-      default:
-        return <div className={sideliststyle.sideList}></div>;
-    }
-  };
-
-  return <div>{renderContent()}</div>;
+      return <div className={sideliststyle.sideList}></div>;
+};
 
    
-  };
+
   
   export default Sidelist;
 
