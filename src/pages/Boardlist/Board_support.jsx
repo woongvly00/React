1ea41ef5176874895f6bd2c52df9374c2f2b28ad
HJM <jungmzzang@naver.com>
2025-04-17 11:06:27 +0900

    일정/예약 디테일 수정 , 게시판 링크 수정

--- src/pages/Boardlist/Board_support.jsx
+++ src/pages/Boardlist/Board_support.jsx
@@ -109,7 +109,7 @@ const Board_support = () => {
     const increaseViewCount = (post_id) => {
         axios.get(`http://10.5.5.12/board/increaseViewCount/${post_id}`)
             .then(() => {
-                navigate(`/mainpage/maincontent/titlelink/${post_id}`);
+                navigate(`/mainpage/maincontent/board/titlelink/${post_id}`);
             })
             .catch(error => {
                 console.error('조회수 증가 실패:', error);
@@ -194,7 +194,7 @@ const Board_support = () => {
 
                     <div className={bstyle.writeButton}>
                         <Link
-                            to="/mainpage/maincontent/write_button"
+                            to="/mainpage/maincontent/board/write_button"
                             state={{ boardId: numericBoardId }}
                         >
                             <button>작성하기</button>
